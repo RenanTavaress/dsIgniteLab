2a92ad0909419af5467fd9ef37ee303209f74761
Renan Tavares <renan.tavares@vr.com.br>
2022-10-18 12:13:41 -0300

    primeiro commit

---
+++ src/components/CheckBox.stories.tsx
@@ -0,0 +1,23 @@
+import { CheckBox, CheckBoxProps } from "./CheckBox";
+import { Meta, StoryObj } from "@storybook/react";
+import { Text } from "./Text";
+
+
+export default {
+	title: "Componets/CheckBox",
+	component: CheckBox,
+	args: {},
+	argTypes: {},
+   decorators: [
+      (Story) => {
+         return(
+            <div className="flex items-center gap-2">
+               {Story()}
+               <Text size="sm" >Lembrar-me de mim por 30 dias</Text>
+            </div>
+         )
+      }
+   ]
+} as Meta<CheckBoxProps>;
+
+export const Default: StoryObj<CheckBoxProps> = {};
